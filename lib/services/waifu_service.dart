d453c93adce71a27d101d5af2b8abc321c62c58f
jeapi <abstractdev7@gmail.com>
2026-04-22 17:48:21 +0200

    removing service images

--- lib/services/waifu_service.dart
+++
@@ -1,9 +0,0 @@
-abstract class WaifuService {
-  String get name;
-  Future<String?> fetchWaifuImage(String tag, {required bool isNsfw});
-  List<String> getSfwTags();
-  List<String> getNsfwTags();
-
-  @override
-  String toString() => name;
-}
